e3a16dfdd5f6b5fcc660a865fae72707421bafbb
thinkty <thinkty@naver.com>
2020-04-11 00:04:11 -0400

    Responsive UI modification

--- src/components/MainFeedPage/MainFeedPage.js
+++ src/components/MainFeedPage/MainFeedPage.js
@@ -1,140 +1,142 @@
 /**
  * This is the main page to display all the notes that the user currently has created
  * Upon mounting, the client should make a request to the server for the current user's notes
- * 
+ *
  */
 
-import React, { Component } from 'react';
-import Topbar from '../Topbar';
-import { AppBar, Grid } from '@material-ui/core';
-import Note from '../Note';
+import React, { Component } from "react";
+import Topbar from "../Topbar";
+import { AppBar, Grid } from "@material-ui/core";
+import Note from "../Note";
 
 export default class MainFeedPage extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    // notes    = array of note components
+    // notesMap = array of keys for notes for accessing the note easibly
+    this.state = {
+      notes: [],
+      notesMap: [],
+    };
+  }
 
-        // notes    = array of note components
-        // notesMap = array of keys for notes for accessing the note easibly
-        this.state = {
-            notes: [],
-            notesMap: []
-        }
+  /**
+   * Helper function to remove the child component (note)
+   * TODO: replace time with _id
+   *
+   * @param  time  currently, we are distinguishing note by time (TODO: use _id)
+   */
+  detonate = (time) => {
+    let notes = this.state.notes;
+    let notesMap = this.state.notesMap;
+
+    // find the note with the correct key
+    for (let i = 0; i < notesMap.length; i++) {
+      if (notesMap[i] === time.toString()) {
+        // remove the current index from both map and array
+        notesMap.splice(i, 1);
+        notes.splice(i, 1);
+        break;
+      }
     }
 
-    /**
-     * Helper function to remove the child component (note)
-     * TODO: replace time with _id
-     * 
-     * @param  time  currently, we are distinguishing note by time (TODO: use _id)
-     */
-    detonate = (time) => {
-        let notes = this.state.notes;
-        let notesMap = this.state.notesMap;
+    // update the state
+    this.setState({
+      notes: notes,
+      notesMap: notesMap,
+    });
+  };
 
-        // find the note with the correct key
-        for (let i = 0; i < notesMap.length; i++) {
-            if (notesMap[i] === time.toString()) {
-                // remove the current index from both map and array
-                notesMap.splice(i, 1);
-                notes.splice(i, 1);
-                break;
-            }
-        }
+  /**
+   * Make a request to the server for notes of the current user
+   */
+  componentDidMount() {
+    //TODO: Fetch from remote database
 
-        // update the state
-        this.setState({
-            notes: notes,
-            notesMap: notesMap
-        });
-    }
+    // TODO: remove mock data
+    let notes = [];
+    let notesMap = [];
+    let mockdata = require("../../mock.json");
+    mockdata.forEach((note) => {
+      // TODO: Replace time with _id
+      notes.push(
+        <Note key={note.time.toString()} data={note} detonate={this.detonate} />
+      );
+      notesMap.push(note.time.toString());
+    });
+    this.setState({
+      notes: notes,
+      notesMap: notesMap,
+    });
+  }
 
-    /**
-     * Make a request to the server for notes of the current user
-     */
-    componentDidMount() {
-        
-        //TODO: Fetch from remote database
+  /**
+   * This is a helper function that will be passed on to Topbar
+   * Topbar will call this function to add the notes visually
+   * and also to the server
+   *
+   * @param  title     title of the new note
+   * @param  content   content of new note
+   * @param  lantool   lantool related to new note
+   * @param  reference reference related to new note
+   */
+  createNewNote = async (title, content, lantool, reference) => {
 
-        // TODO: remove mock data
-        let notes = [];
-        let notesMap = [];
-        let mockdata = require('../../mock.json');
-        mockdata.forEach(note => {
-            // TODO: Replace time with _id
-            notes.push(
-                <Note key={note.time.toString()} data={note} detonate={this.detonate}/>
-            )
-            notesMap.push(note.time.toString());
-        });
-        this.setState({
-            notes: notes,
-            notesMap: notesMap
-        });
-    }
+    let noteData = {
+      title: title,
+      content: content,
+      lantool: lantool,
+      time: new Date().getTime(),
+      reference: reference,
+    };
+    // TODO: Send request to the server to add new note
+    // TODO: do it synchronously
 
-    /**
-     * This is a helper function that will be passed on to Topbar
-     * Topbar will call this function to add the notes visually
-     * and also to the server
-     * 
-     * @param  title     title of the new note
-     * @param  content   content of new note
-     * @param  lantool   lantool related to new note
-     * @param  reference reference related to new note
-     */
-    createNewNote = async (title, content, lantool, reference) => {
+    // fetch the _id of the new note
+    let _id = 69;
 
-        // TODO: Send request to the server to add new note
-        // TODO: do it synchronously
+    // graphically update the notes
+    // put the _id also as it is needed for modifying the ntoe
+    noteData.id = _id;
 
-        // fetch the _id of the new note
-        let _id = 69;
+    // add the new note to the state
+    let notes = this.state.notes;
+    notes.push(
+      <Note
+        key={noteData.time.toString()}
+        data={noteData}
+        detonate={this.detonate}
+      />
+    );
+    // add the key also to the map
+    let map = this.state.notesMap;
+    map.push(noteData.time.toString()); // TODO: replace to id
 
-        // graphically update the notes
-        // put the _id also as it is needed for modifying the ntoe
-        let noteData = {
-            id : _id,
-            title : title,
-            content : content,
-            lantool : lantool,
-            time : new Date().getTime(),
-            reference : reference
-        };
-        
-        // add the new note to the state
-        let notes = this.state.notes;
-        notes.push(<Note key={noteData.time.toString()} data={noteData} detonate={this.detonate}/>);
-        // add the key also to the map
-        let map = this.state.notesMap;
-        map.push(noteData.time.toString()); // TODO: replace to id
+    // setState to rerender
+    this.setState({
+      notes: notes,
+      notesMap: map,
+    });
+  };
 
-        // setState to rerender
-        this.setState({
-            notes: notes,
-            notesMap: map
-        });
-    }
-    
-    render() {
-        return (
-            <div>
-                <AppBar
-                color="transparent"
-                >
-                    <Topbar createNewNote={this.createNewNote}/>
-                </AppBar>
-                <br/><br/><br/><br/><br/>
-                <Grid
-                container
-                direction="column-reverse"
-                justify="center"
-                alignItems="center"
-                spacing={1}
-                >
-                    {this.state.notes}
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <AppBar color="transparent">
+          <Topbar createNewNote={this.createNewNote} />
+        </AppBar>
+        <Grid
+          container
+          direction="column-reverse"
+          justify="center"
+          alignItems="center"
+          spacing={1}
+          style={{marginTop: "5em"}}
+        >
+          {this.state.notes}
+        </Grid>
+      </div>
+    );
+  }
 }
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -8,318 +8,343 @@
  *  - reference
  */
 
-import React, { Component } from 'react';
-import { 
-    Grid, 
-    Typography, 
-    IconButton, 
-    Card, 
-    CardHeader, 
-    Tooltip, 
-    Menu, 
-    MenuItem,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    TextField,
-    Button,
-    CardContent,
- } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import Icon from '../Icons';
-import Reference from './Reference';
+import React, { Component } from "react";
+import {
+  Grid,
+  Typography,
+  IconButton,
+  Card,
+  CardHeader,
+  Tooltip,
+  Menu,
+  MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+  CardContent,
+  Divider,
+} from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import Icon from "../Icons";
+import Reference from "./Reference";
 
-const iconList = require('../Icons/list.json');
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+const iconList = require("../Icons/list.json");
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export default class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.data.title,
+      content: props.data.content,
+      lantool: props.data.lantool,
+      tempTitle: props.data.title,
+      tempContent: props.data.content,
+      tempLantool: props.data.lantool,
+      tempReference: props.data.reference,
+      time: props.data.time,
+      formattedTime: "",
+      reference: props.data.reference,
+      openSubmenu: null,
+      isEditNote: false,
+      detonate: props.detonate,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: props.data.title,
-            content: props.data.content,
-            lantool: props.data.lantool,
-            tempTitle: props.data.title,
-            tempContent: props.data.content,
-            tempLantool: props.data.lantool,
-            tempReference: props.data.reference,
-            time: props.data.time,
-            formattedTime: "",
-            reference: props.data.reference,
-            openSubmenu: null,
-            isEditNote: false,
-            detonate: props.detonate,
-        };
+  /**
+   * Helper function to get the time format from the given time in miliseconds
+   */
+  getTime = (time) => {
+    let date = new Date(time);
+    return (
+      monthNames[date.getMonth()] +
+      " " +
+      date.getDate() +
+      ", " +
+      date.getFullYear()
+    );
+  };
+
+  /**
+   * Helper function to toggle the submenu when clicking on the top right menu icon
+   */
+  toggleSubmenu = (event) => {
+    if (this.state.openSubmenu === null) {
+      this.setState({
+        openSubmenu: event.currentTarget,
+      });
+    } else {
+      this.setState({
+        openSubmenu: null,
+      });
+    }
+  };
+
+  /**
+   * Helper function to remove the note
+   */
+  removeNote = () => {
+    // TODO: Make call to the server
+
+    // destroy self
+    this.state.detonate(this.state.time);
+    this.toggleSubmenu();
+  };
+
+  /**
+   * Helper function to edit the note
+   */
+  editNote = () => {
+    // open the edit note dialog with prefilled information
+    this.setState({
+      isEditNote: !this.state.isEditNote,
+    });
+  };
+
+  /**
+   * Handler for changes on new note creation
+   */
+  onTempNoteChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  /**
+   * Special case for handling lantool change
+   */
+  onLantoolChange = (value) => {
+    this.setState({
+      tempLantool: value,
+    });
+  };
+
+  /**
+   * Handler for submitting the editted note
+   */
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    // Make sure none of the details are empty
+    if (this.state.tempTitle === "") {
+      alert("Title cannot be empty");
+      return;
+    }
+    if (this.state.tempContent === "") {
+      alert("Content cannot be empty");
+      return;
+    }
+    if (this.state.tempLantool === "") {
+      alert("Please choose a language or tool");
+      return;
     }
 
-    /**
-     * Helper function to get the time format from the given time in miliseconds
-     */
-    getTime = (time) => {
-        let date = new Date(time);
-        return (monthNames[date.getMonth()]) + " " + date.getDate() + ", " + date.getFullYear();
-    }
+    // update the current title, content, lantool, reference
+    this.setState({
+      title: this.state.tempTitle,
+      content: this.state.tempContent,
+      lantool: this.state.tempLantool,
+      reference: this.state.tempReference,
+    });
 
-    /**
-     * Helper function to toggle the submenu when clicking on the top right menu icon
-     */
-    toggleSubmenu = (event) => {
-        if (this.state.openSubmenu === null) {
-            this.setState({
-                openSubmenu: event.currentTarget
-            });
-        } else {
-            this.setState({
-                openSubmenu: null
-            });
-        }
-    }
+    // TODO: Send updated data to the database
 
-    /**
-     * Helper function to remove the note
-     */
-    removeNote = () => {
-        
-        // TODO: Make call to the server
+    // close the dialog
+    this.editNote();
+    // close the submenu
+    this.toggleSubmenu();
+  };
 
-        // destroy self
-        this.state.detonate(this.state.time);
-        this.toggleSubmenu();
-    }
+  /**
+   * Handler for cancelling the creation of the new note
+   */
+  onCancel = () => {
+    // close the dialog
+    this.editNote();
+    // close the submenu
+    this.toggleSubmenu();
+  };
 
-    /**
-     * Helper function to edit the note
-     */
-    editNote = () => {
+  render() {
+    return (
+      <Grid
+        item
+        xs={12}
+        style={{ width: "80%", minWidth: "360px", maxWidth: "800px" }}
+      >
+        <Card>
+          <CardHeader
+            avatar={<Icon name={this.state.lantool} />}
+            action={
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                style={{marginTop: "20%"}}
+              >
+                <Grid item>
+                  <Tooltip
+                    title="Note Menu"
+                    placement="right"
+                    arrow
+                    enterDelay={500}
+                  >
+                    <IconButton onClick={this.toggleSubmenu}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 25 25"
+                        width="30px"
+                        height="30px"
+                      >
+                        <line
+                          x1="10"
+                          y1="10"
+                          x2="15"
+                          y2="10"
+                          stroke="#6698FA"
+                          strokeWidth="5"
+                        />
+                        <line
+                          x1="10"
+                          y1="20"
+                          x2="15"
+                          y2="20"
+                          stroke="#6698FA"
+                          strokeWidth="5"
+                        />
+                      </svg>
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    anchorEl={this.state.openSubmenu}
+                    keepMounted
+                    open={Boolean(this.state.openSubmenu)}
+                    onClose={this.toggleSubmenu}
+                  >
+                    <MenuItem onClick={this.removeNote}>Remove</MenuItem>
+                    <MenuItem onClick={this.editNote}>Edit</MenuItem>
+                  </Menu>
+                </Grid>
+              </Grid>
+            }
+            title={this.state.title}
+            subheader={this.getTime(this.state.time)}
+            style={{marginBottom: "-3%", marginTop: "-2%"}}
+          />
+          <Divider/>
+          <CardContent>
+            <Typography variant="body2">{this.state.content}</Typography>
+          </CardContent>
+          {this.state.reference === "" ? (
+            <div />
+          ) : (
+            <DialogActions>
+              <Reference link={this.state.reference} />
+            </DialogActions>
+          )}
+        </Card>
 
-        // open the edit note dialog with prefilled information
-        this.setState({
-            isEditNote: !this.state.isEditNote
-        });
-    }
-
-    /**
-     * Handler for changes on new note creation
-     */
-    onTempNoteChange = (event) => {
-        this.setState({
-            [event.target.id] : event.target.value
-        });
-    }
-    
-    /**
-     * Special case for handling lantool change
-     */
-    onLantoolChange = (value) => {
-        this.setState({
-            tempLantool : value
-        });
-    }
-
-    /**
-     * Handler for submitting the new note
-     */
-    onSubmit = () => {
-        // Make sure none of the details are empty
-        if (this.state.tempTitle === "") {
-            alert("Title cannot be empty");
-            return;
-        }
-        if (this.state.tempContent === "") {
-            alert("Content cannot be empty");
-            return;
-        }
-        if (this.state.tempLantool === "") {
-            alert("Please choose a language or tool");
-            return;
-        }
-
-        // update the current title, content, lantool, reference
-        this.setState({
-            title: this.state.tempTitle,
-            content: this.state.tempContent,
-            lantool: this.state.tempLantool,
-            reference: this.state.tempReference
-        });
-
-        // TODO: Send updated data to the database
-
-        // close the dialog
-        this.editNote();
-        // close the submenu
-        this.toggleSubmenu();
-    }
-
-    /**
-     * Handler for cancelling the creation of the new note
-     */
-    onCancel = () => {
-        // close the dialog
-        this.editNote();
-        // close the submenu
-        this.toggleSubmenu();
-    }
-
-
-    render() {
-        return (
-            <Grid
-            item 
-            xs={12}
-            style={{width: "80%", minWidth: "360px", maxWidth: "800px"}}
+        <Dialog
+          open={this.state.isEditNote}
+          onClose={() => {
+            this.editNote();
+            this.toggleSubmenu();
+          }}
+        >
+          <DialogTitle>
+            <Typography>Edit Note</Typography>
+          </DialogTitle>
+          <DialogContent dividers>
+            <TextField
+              autoFocus
+              id="tempTitle"
+              required
+              color="secondary"
+              margin="normal"
+              label="Title"
+              type="text"
+              fullWidth
+              onChange={this.onTempNoteChange}
+              value={this.state.tempTitle}
+            />
+            <TextField
+              id="tempContent"
+              required
+              variant="outlined"
+              color="secondary"
+              margin="normal"
+              label="Content"
+              type="text"
+              fullWidth
+              multiline
+              rows="3"
+              rowsMax="10"
+              onChange={this.onTempNoteChange}
+              value={this.state.tempContent}
+            />
+            <Autocomplete
+              id="lantool"
+              autoHighlight
+              options={iconList}
+              getOptionLabel={(option) => option.title}
+              onChange={(event, newValue) => {
+                if (newValue == null) {
+                  this.onLantoolChange("");
+                  return;
+                }
+                this.onLantoolChange(newValue.title);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  label="Language or Tool"
+                  color="secondary"
+                />
+              )}
+            />
+            <TextField
+              id="tempReference"
+              color="secondary"
+              margin="normal"
+              label="Reference"
+              type="text"
+              fullWidth
+              onChange={this.onTempNoteChange}
+              value={this.state.tempReference}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button variant="outlined" onClick={this.onCancel} color="inherit">
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              onClick={this.onSubmit}
+              color="secondary"
             >
-                <Card>
-                    <CardHeader 
-                    avatar={
-                        <Icon name={this.state.lantool}/>
-                    }
-                    action={
-                        <Grid
-                        container
-                        direction="column"
-                        justify="center"
-                        alignItems="center"
-                        >
-                            <Grid
-                            item
-                            >
-                                <Tooltip
-                                title="Note Menu"
-                                placement="right"
-                                arrow
-                                enterDelay={500}
-                                >
-                                    <IconButton
-                                    onClick={this.toggleSubmenu}
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" width="30px" height="30px">
-                                            <line x1="10" y1="10" x2="15" y2="10" stroke="#6698FA" strokeWidth="5" />
-                                            <line x1="10" y1="20" x2="15" y2="20" stroke="#6698FA" strokeWidth="5" />
-                                        </svg>
-                                    </IconButton>
-                                </Tooltip>
-                                <Menu
-                                anchorEl={this.state.openSubmenu}
-                                keepMounted
-                                open={Boolean(this.state.openSubmenu)}
-                                onClose={this.toggleSubmenu}
-                                >
-                                    <MenuItem
-                                    onClick={this.removeNote}
-                                    >
-                                        Remove
-                                    </MenuItem>
-                                    <MenuItem
-                                    onClick={this.editNote}
-                                    >
-                                        Edit
-                                    </MenuItem>
-                                </Menu>
-                            </Grid>
-                        </Grid>
-                    }
-                    title={this.state.title}
-                    subheader={this.getTime(this.state.time)}
-                    />
-                    <CardContent>
-                        <Typography
-                        variant="body2"
-                        >
-                            {this.state.content}
-                        </Typography>
-                    </CardContent>
-                    {this.state.reference === "" ? <div/> : <DialogActions><Reference link={this.state.reference}/></DialogActions>}
-                </Card>
-
-                <Dialog 
-                open={this.state.isEditNote}
-                onClose={() => {this.editNote(); this.toggleSubmenu()}}
-                >
-                    <DialogTitle>
-                        <Typography
-                        >
-                            Edit Note
-                        </Typography>
-                    </DialogTitle>
-                    <DialogContent
-                    dividers
-                    >
-                        <TextField
-                        autoFocus
-                        id="tempTitle"
-                        required
-                        color="secondary"
-                        margin="normal"
-                        label="Title"
-                        type="text"
-                        fullWidth
-                        onChange={this.onTempNoteChange}
-                        value={this.state.tempTitle}
-                        />
-                        <TextField
-                        id="tempContent"
-                        required
-                        variant="outlined"
-                        color="secondary"
-                        margin="normal"
-                        label="Content"
-                        type="text"
-                        fullWidth
-                        multiline
-                        rows="3"
-                        rowsMax="10"
-                        onChange={this.onTempNoteChange}
-                        value={this.state.tempContent}
-                        />
-                        <Autocomplete
-                        id="lantool"
-                        autoHighlight
-                        options={iconList}
-                        getOptionLabel={option => option.title}
-                        onChange={(event, newValue) => {
-                            if (newValue == null) {
-                                this.onLantoolChange("");
-                                return;
-                            }
-                            this.onLantoolChange(newValue.title);
-                        }}
-                        renderInput={params => <TextField {...params} 
-                            required
-                            label="Language or Tool"
-                            color="secondary"
-                            />}
-                        />
-                        <TextField
-                        id="tempReference"
-                        color="secondary"
-                        margin="normal"
-                        label="Reference"
-                        type="text"
-                        fullWidth
-                        onChange={this.onTempNoteChange}
-                        value={this.state.tempReference}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button
-                        variant="outlined"
-                        onClick={this.onCancel}
-                        color="inherit"
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                        variant="contained"
-                        onClick={this.onSubmit}
-                        color="secondary"
-                        >
-                            Edit
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </Grid>
-        );
-    }
+              Edit
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>
+    );
+  }
 }
